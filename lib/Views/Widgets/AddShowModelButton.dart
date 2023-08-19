1fae7ad782cbf2a6116c7fbd260a87c1833bc6f8
m7md3bbas <mhmdabass330@gmail.com>
2023-08-19 19:18:59 +0300

    add textFiled to notesButtonSheet

--- lib/Views/Widgets/AddShowModelButton.dart
+++ lib/Views/Widgets/AddShowModelButton.dart
@@ -5,6 +5,45 @@ class AddNoteModelSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        CustomTextFild(
+          title: 'Title',
+        ),
+        CustomTextFild(
+          title: 'Content',
+          maxLines: 5,
+        )
+      ],
+    );
   }
+}
+
+class CustomTextFild extends StatelessWidget {
+  CustomTextFild({super.key, this.maxLines, this.title});
+  int? maxLines;
+  String? title;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        maxLines: maxLines,
+        cursorColor: Color(0xff4c7f7d),
+        decoration: InputDecoration(
+            hintText: title,
+            hintStyle: TextStyle(
+                color: Color(
+                  0xff4c7f7d,
+                ),
+                fontSize: 20),
+            enabledBorder: CustomBorderTextfiled(Color: Colors.white),
+            focusedBorder: CustomBorderTextfiled(Color: Color(0xff4c7f7d))),
+      ),
+    );
+  }
+
+  OutlineInputBorder CustomBorderTextfiled({Color}) => OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: Color));
 }
