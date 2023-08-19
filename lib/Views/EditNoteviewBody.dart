526d39350866971842a39d1a3d3f71c383647036
m7md3bbas <mhmdabass330@gmail.com>
2023-08-20 00:44:31 +0300

    create edit view body

---
+++ lib/Views/EditNoteviewBody.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:notesapp/Views/Widgets/EditNoteView.dart';
+
+class EditViewBody extends StatelessWidget {
+  const EditViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return EditNoteView();
+  }
+}
