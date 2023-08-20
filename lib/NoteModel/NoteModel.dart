022ffb004e48d6126cfeaeee20197260488bf526
m7md3bbas <mhmdabass330@gmail.com>
2023-08-20 03:00:19 +0300

    create type adapter for note

--- lib/NoteModel/NoteModel.dart
+++ lib/NoteModel/NoteModel.dart
@@ -1,7 +1,16 @@
-class NoteModel {
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+part 'NoteModel.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveType(typeId: 0)
   String title;
+  @HiveType(typeId: 1)
   String subTitle;
+  @HiveType(typeId: 2)
   String date;
+  @HiveType(typeId: 3)
   int color;
 
   NoteModel(
---
+++ lib/NoteModel/NoteModel.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'NoteModel.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class NoteModelAdapter extends TypeAdapter<NoteModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  NoteModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return NoteModel(
+        title: fields[0] as String,
+        subTitle: fields[1] as String,
+        color: fields[2] as int,
+        date: fields[1] as String);
+  }
+
+  @override
+  void write(BinaryWriter writer, NoteModel obj) {
+    writer.writeByte(0);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NoteModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
