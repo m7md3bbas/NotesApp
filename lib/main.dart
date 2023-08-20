576eb6fb89888e1710dfe25b29805ecced58a6b2
m7md3bbas <mhmdabass330@gmail.com>
2023-08-20 03:06:11 +0300

    add hive init and hive box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notesapp/Views/Widgets/CustomNotesItems.dart';
 import 'Views/NotesViews.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('notes box');
   runApp(const NotesApp());
 }
 
